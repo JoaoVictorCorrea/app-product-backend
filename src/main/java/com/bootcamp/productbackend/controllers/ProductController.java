8db6db88d86fd8ca99c4131184565ee973e60b61
João Victor Correa <correa.joaov25@gmail.com>
2024-02-08 17:20:02 -0300

    Seeding Products and Configurated the endpoint 'getProducts'

--- src/main/java/com/bootcamp/productbackend/controllers/ProductController.java
+++ src/main/java/com/bootcamp/productbackend/controllers/ProductController.java
@@ -1,57 +1,53 @@
 package com.bootcamp.productbackend.controllers;
 
-import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bootcamp.productbackend.models.Product;
+import com.bootcamp.productbackend.repositories.ProductRepository;
 
 @RestController
 @CrossOrigin
 public class ProductController {
 
-    private List<Product> products = new ArrayList<>();
-    
-    @GetMapping("products/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+    //private List<Product> products = new ArrayList<>();
 
-        Product product = products.stream()
-                                  .filter(p -> p.getId() == id)
-                                  .findFirst()
-                                  .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+    @Autowired
+    private ProductRepository productRepository;
+    
+    // @GetMapping("products/{id}")
+    // public ResponseEntity<Product> getProduct(@PathVariable int id) {
+
+    //     Product product = products.stream()
+    //                               .filter(p -> p.getId() == id)
+    //                               .findFirst()
+    //                               .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
                                   
-        return ResponseEntity.ok(product);
-    }
+    //     return ResponseEntity.ok(product);
+    // }
     
     @GetMapping("products")
     public List<Product> getProducts() {
 
-        return products;
+        return productRepository.findAll();
     }
 
-    @PostMapping("products")
-    public ResponseEntity<Product> save(@RequestBody Product product) {
+    // @PostMapping("products")
+    // public ResponseEntity<Product> save(@RequestBody Product product) {
         
-        product.setId(products.size() + 1);
-        products.add(product);
+    //     product.setId(products.size() + 1);
+    //     products.add(product);
 
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(product.getId())
-                .toUri();
+    //     URI location = ServletUriComponentsBuilder
+    //             .fromCurrentRequest()
+    //             .path("/{id}")
+    //             .buildAndExpand(product.getId())
+    //             .toUri();
 
-        return ResponseEntity.created(location).body(product);
-    }
+    //     return ResponseEntity.created(location).body(product);
+    // }
 }
